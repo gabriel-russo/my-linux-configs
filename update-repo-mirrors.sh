724f1cc655aabe59e59be4314f5f831ac6b9b5a0
Gabriel Russo <gabrielrusso@pm.me>
2022-05-26 00:13:19 -0400

    Script to update all mirror's URL v1

---
+++ update-repo-mirrors.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# update-repo-mirrors.sh - Update all /usr/share/xbps.d/ mirrors's URL
+#
+# Site : www.github.com/gabriel-russo
+# Autor: Gabriel Russo <gabrielrusso@pm.me>
+#
+# -------------------------------------------------------------
+#
+# This program receive a <URL> by parameter and update all available files
+# in /usr/share/xbps.d with repository=<URL>/current/{nonfree, multilib} ...
+#
+# Examples:
+# $ ./update-repo-mirrors.sh https://repo-us.voidlinux.org  
+#
+# --------------------------------------------------------------
+#
+# License: Unlicense
+
+echo "Starting script..."
+scaped_url="${1//\//\\\/}"
+
+echo
+read -p "Modifying all URLs with: ${1} , Continue? [y/n]: " continue
+
+# ${continue,,} Only works with bash >= 4.0
+if [ "${continue,,}" = 'n' ]; then
+	exit 1
+fi
+
+XBPS_D=/usr/share/xbps.d
+
+echo
+echo "Entering directory xbps.d -> ${XBPS_D}"
+echo
+
+mirror_conf_files=("00-repository-main.conf" "10-repository-nonfree.conf" "10-repository-multilib.conf" "10-repository-multilib-nonfree.conf" )
+
+for conf_file in "${mirror_conf_files[@]}"; 
+do
+	if [ -e $XBPS_D/$conf_file ]; then
+		echo "=============//=============="
+		echo "Modifying file: ${conf_file}"
+		sudo sed -i -r "s/[^=]*[[:alnum:]\-\.]\.(org|com)/$scaped_url/g" $XBPS_D/$conf_file
+		echo
+		echo "File modified! Checking with cat command: $(cat $XBPS_D/$conf_file)"
+		echo "=============//=============="
+		echo
+	fi
+done
+
